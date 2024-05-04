01da512af7699480f58ddd7ebcca4cfbf49fcb35
Alnick Clores <aelnickclores@gmail.com>
2024-05-05 01:49:24 +0800

    adds confirmation before downloading

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener('click', () => {
-
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
 
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-
-}));
+document
+  .getElementById("downloadButton")
+  .addEventListener("click", function () {
+    var confirmation = confirm("Are you sure you want to download the app?");
+    if (!confirmation) {
+      event.preventDefault(); // Prevent the default download behavior if the user cancels
+    }
+  });
